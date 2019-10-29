59a8defce08dc407f6fe4956dd31078760f53390
Alex Shearer <alex.shearer23@gmail.com>
2019-10-29 09:54:16 -0500

    Added styling to main scss. Also added in the calculation to update the step class into our JS

--- js/script.js
+++ js/script.js
@@ -60,11 +60,24 @@ function Clock() {
             result += (hours + ":");
         }
         result += (addLeadingZeroes(minutes) + ":" + addLeadingZeroes(seconds));
-        return result
+        return result;
 
     }
     this.displayCurrentTime = function () {
         $('.main-display').text(formatTime(currentTime));
+
+        //Update the class for the progress radial to be either break or session depending on what mode we're in
+        if (mode === "Session" && $('.progress-radial').hasClass('break')) {
+            $('.progress-radial').removeClass('break').addClass('session');
+        } else if (mode === "Breal" && $('.progress-radial').hasClass('session')) {
+            $('.progress-radial').removeClass('session').addClass('break');
+        }
+
+        //Set up the step class for the radial
+        $('.progress-radial').attr('class', function(index, currentValue) {
+            return currentValue.replace(/(^|\s)step-\S+/g, " step-" + (100 - parseInt((currentTime / startTime) * 100)));
+        });
+        console.log($('.progress-radial').attr('class'));
     }
 
     //Function to display the session time
